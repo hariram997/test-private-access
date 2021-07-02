96ec87387a6c8a1d3f740af2e806f4062baa9662
Raghul S <raghulsenthil6@gmail.com>
2021-07-02 16:57:13 +0530

    Refactor. Removed workspace path

--- .ignore/go-gitpod.sh
+++ .ignore/go-gitpod.sh
@@ -5,7 +5,6 @@ cd $DIR
 curl -s --output guru-shifu.tar.gz "https://guru-shifu-artifacts.s3.ap-south-1.amazonaws.com/Guru-Shifu-Trainee-Output/outputDev/guru-shifu-neev-dev.tar.gz" 
 tar -xf guru-shifu.tar.gz
 touch .env
-echo 'WORKSPACE_PATH=/workspace/gitpod' >> .env
 echo "REACT_APP_HOST_URL=https://8080-${GITPOD_WORKSPACE_URL#*//}" >> .env
 echo "Loading guru-shifu images..."
 docker load -i guru-shifu-images.tar.gz
