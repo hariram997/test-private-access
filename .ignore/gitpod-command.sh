2a912fb79fd79b446f71c53783730b4e368d4734
Mohankumar T <mohan13081999@gmail.com>
2021-07-14 13:09:03 +0530

    Add. Command to swith to source directory of script

--- .ignore/gitpod-command.sh
+++ .ignore/gitpod-command.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $DIR
 echo "Starting guru-shifu..."
 docker-compose -f docker-compose-gitpod.yml up -d
 if [ $? == 0 ]
