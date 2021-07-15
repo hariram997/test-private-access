978b2216bcf3226fd57faf171ceda15f926789e3
Mohankumar T <mohan13081999@gmail.com>
2021-07-15 09:57:32 +0530

    Refactor. Use artifact URL based on environment

--- .ignore/init.sh
+++ .ignore/init.sh
@@ -2,7 +2,15 @@
 echo "Locating required resources..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $DIR
-curl -s --output guru-shifu.tar.gz "https://guru-shifu-artifacts.s3.ap-south-1.amazonaws.com/Guru-Shifu-Trainee-Output/outputDev/guru-shifu-twi-dev-gitpod.tar.gz" 
+if [ $STAGE == "prod" ]
+then
+    echo "Using Prod Artifact..."
+    ARTIFACT_URL="https://guru-shifu-artifacts.s3.ap-south-1.amazonaws.com/Guru-Shifu-Trainee-Output/outputProd/guru-shifu-twi-prod-gitpod.tar.gz"
+else
+    echo "Using Dev Artifact..."
+    ARTIFACT_URL="https://guru-shifu-artifacts.s3.ap-south-1.amazonaws.com/Guru-Shifu-Trainee-Output/outputDev/guru-shifu-twi-dev-gitpod.tar.gz"
+fi
+curl -s --output guru-shifu.tar.gz "$ARTIFACT_URL"
 tar -xf guru-shifu.tar.gz
 mkdir /workspace/gitpod/m2-repository
 printf '<settings>\n  <localRepository>/workspace/gitpod/m2-repository/</localRepository>\n</settings>\n' > /home/gitpod/.m2/settings.xml
