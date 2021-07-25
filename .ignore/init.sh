7e00bf4d578c3c182941f88d97c2dec24a055743
Mohankumar T <mohan13081999@gmail.com>
2021-07-25 16:30:33 +0530

    Refactor. Updated log messages with timestamp

--- .ignore/init.sh
+++ .ignore/init.sh
@@ -4,20 +4,20 @@ timestamp(){
     date
 }
 
-echo "Locating required resources..."
+echo "$(timestamp) Locating required resources..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $DIR
 if [ $STAGE == "prod" ]
 then
-    echo "Using Prod Artifact..."
+    echo "$(timestamp) Using Prod Artifact..."
     ARTIFACT_URL="https://guru-shifu-artifacts.s3.ap-south-1.amazonaws.com/Guru-Shifu-Trainee-Output/outputProd/guru-shifu-twi-prod-gitpod.tar.gz"
 else
-    echo "Using Dev Artifact..."
+    echo "$(timestamp) Using Dev Artifact..."
     ARTIFACT_URL="https://guru-shifu-artifacts.s3.ap-south-1.amazonaws.com/Guru-Shifu-Trainee-Output/outputDev/guru-shifu-twi-dev-gitpod.tar.gz"
 fi
 curl -s --output guru-shifu.tar.gz "$ARTIFACT_URL"
 tar -xf guru-shifu.tar.gz
 mkdir /workspace/gitpod/m2-repository
 printf '<settings>\n  <localRepository>/workspace/gitpod/m2-repository/</localRepository>\n</settings>\n' > /home/gitpod/.m2/settings.xml
-echo "Loading guru-shifu images..."
+echo "$(timestamp) Loading guru-shifu images..."
 docker load -i guru-shifu-images.tar.gz
