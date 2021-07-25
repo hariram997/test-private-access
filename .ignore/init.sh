93b9c6527de269b525423423df99eaad8e93ab22
Mohankumar T <mohan13081999@gmail.com>
2021-07-25 16:35:51 +0530

    Add. Additional log messages

--- .ignore/init.sh
+++ .ignore/init.sh
@@ -15,9 +15,14 @@ else
     echo "$(timestamp) Using Dev Artifact..."
     ARTIFACT_URL="https://guru-shifu-artifacts.s3.ap-south-1.amazonaws.com/Guru-Shifu-Trainee-Output/outputDev/guru-shifu-twi-dev-gitpod.tar.gz"
 fi
+echo "$(timestamp) Starting download of artifact ..."
 curl -s --output guru-shifu.tar.gz "$ARTIFACT_URL"
+echo "$(timestamp) Artifact download complete."
+echo "$(timestamp) Unzipping guru-shifu tarball.."
 tar -xf guru-shifu.tar.gz
+echo "$(timestamp) $(timestamp) Unzip complete"
 mkdir /workspace/gitpod/m2-repository
 printf '<settings>\n  <localRepository>/workspace/gitpod/m2-repository/</localRepository>\n</settings>\n' > /home/gitpod/.m2/settings.xml
 echo "$(timestamp) Loading guru-shifu images..."
 docker load -i guru-shifu-images.tar.gz
+echo "$(timestamp) Guru-shifu images loaded successfully"
