26819bef56759d922bfbae0b193f0ea559b95f56
Mohankumar T <mohan13081999@gmail.com>
2021-07-25 16:51:23 +0530

    Refactor. Log messages with timestamp

--- .ignore/start.sh
+++ .ignore/start.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
+
+timestamp(){
+    date
+}
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $DIR
 touch .env
 echo "REACT_APP_HOST_URL=https://8080-${GITPOD_WORKSPACE_URL#*//}" > .env
-echo "Starting guru-shifu..."
+echo "$(timestamp) Starting guru-shifu..."
 docker-compose -f docker-compose-gitpod.yml up -d
 if [ $? == 0 ]
 then
-  echo "Waiting for guru-shifu to start up.... "
+  echo "$(timestamp) Waiting for guru-shifu to start up.... "
   until $(curl --output /dev/null --silent --head --fail http://localhost:3000/); do
     printf "."
     sleep 1
   done
-  echo "Guru-shifu started successfully..."
+  echo "$(timestamp) Guru-shifu started successfully..."
 fi
 cd /workspace
