6cf9ee089f6c322402acceffa97245682092990b
hariramprasanth balasubramanian <hariramprasanth.b@thoughtworks.com>
2021-11-17 16:20:34 +0530

    Get user details from terminal

--- .ignore/init.sh
+++ .ignore/init.sh
@@ -7,8 +7,9 @@ timestamp(){
 echo "$(timestamp) Locating required resources..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $DIR
-USER_NAME=ashwathi
-PASSWORD=Sweety@1
+echo "Please Enter your Details Below ...."
+read -p "Enter your name : " USER_NAME
+read -s -p "Enter your password : " PASSWORD
 BODY='{"ClientId": "9s990bibbsg26bepjvofubc1g","AuthParameters": {"USERNAME": "'"$USER_NAME"'","PASSWORD": "'"$PASSWORD"'"},"AuthFlow": "USER_PASSWORD_AUTH"}'
 HEADER1='X-Amz-Target: AWSCognitoIdentityProviderService.InitiateAuth'
 HEADER2='Content-Type: application/x-amz-json-1.1'
