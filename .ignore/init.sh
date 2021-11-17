849df752d1ca5054d43446009477103bd5ce2f64
hariramprasanth balasubramanian <hariramprasanth.b@thoughtworks.com>
2021-11-17 11:51:10 +0530

    add url and removed gurushifu artifatcs commands

--- .ignore/init.sh
+++ .ignore/init.sh
@@ -7,27 +7,5 @@ timestamp(){
 echo "$(timestamp) Locating required resources..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $DIR
-echo "$DIR"
-if [ $STAGE == "prod" ]
-then
-    echo "$(timestamp) Using Prod Artifact..."
-    ARTIFACT_URL="https://guru-shifu-artifacts.s3.ap-south-1.amazonaws.com/Guru-Shifu-Trainee-Output/outputProd/guru-shifu-twi-prod-gitpod.tar.gz"
-elif [ $STAGE == "test" ]
-then
-    echo "$(timestamp) Using Test Artifact..."
-    ARTIFACT_URL="https://guru-shifu-artifacts.s3.ap-south-1.amazonaws.com/Guru-Shifu-Trainee-Output/outputTest/guru-shifu-twi-test-gitpod.tar.gz"
-else
-    echo "$(timestamp) Using Dev Artifact..."
-    ARTIFACT_URL="https://guru-shifu-artifacts.s3.ap-south-1.amazonaws.com/Guru-Shifu-Trainee-Output/outputDev/guru-shifu-twi-dev-gitpod.tar.gz"
-fi
-echo "$(timestamp) Starting download of artifact ..."
-curl -s --output guru-shifu.tar.gz "$ARTIFACT_URL"
-echo "$(timestamp) Artifact download complete."
-echo "$(timestamp) Unzipping guru-shifu tarball.."
-tar -xf guru-shifu.tar.gz
-echo "$(timestamp) Unzip complete"
-mkdir /workspace/gitpod/m2-repository
-printf '<settings>\n  <localRepository>/workspace/gitpod/m2-repository/</localRepository>\n</settings>\n' > /home/gitpod/.m2/settings.xml
-echo "$(timestamp) Loading guru-shifu images..."
-docker load -i guru-shifu-images.tar.gz
-echo "$(timestamp) Guru-shifu images loaded successfully"
+export URL=`curl https://2jum04ptde.execute-api.ap-south-1.amazonaws.com/dev`
+curl -s --output myfile.sh $URL
--- .ignore/start.sh
+++ .ignore/start.sh
@@ -3,22 +3,5 @@
 timestamp(){
     date
 }
-printf '<settings>\n  <localRepository>/workspace/gitpod/m2-repository/</localRepository>\n</settings>\n' > /home/gitpod/.m2/settings.xml
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd $DIR
-touch .env
-echo "REACT_APP_HOST_URL=https://8080-${GITPOD_WORKSPACE_URL#*//}" > .env
-echo "$(timestamp) Starting guru-shifu..."
-echo "$(timestamp) Running docker-compose up in detach mode.."
-docker-compose -f docker-compose-gitpod.yml up -d
-echo "$(timestamp) Docker compose completed."
-if [ $? == 0 ]
-then
-  echo "$(timestamp) Waiting for guru-shifu to start up.... "
-  until $(curl --output /dev/null --silent --head --fail http://localhost:3000/); do
-    printf "."
-    sleep 1
-  done
-  echo "$(timestamp) Guru-shifu started successfully..."
-fi
-cd /workspace
+
+echo "Finished.."
