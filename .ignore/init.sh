7e269d13bcd761f427cabd520233bb07a35ea695
hariramprasanth balasubramanian <hariramprasanth.b@thoughtworks.com>
2021-11-19 11:41:10 +0530

    Add header with token Id

--- .ignore/init.sh
+++ .ignore/init.sh
@@ -13,13 +13,13 @@ read -s -p "Enter your password : " PASSWORD
 BODY='{"ClientId": "9s990bibbsg26bepjvofubc1g","AuthParameters": {"USERNAME": "'"$USER_NAME"'","PASSWORD": "'"$PASSWORD"'"},"AuthFlow": "USER_PASSWORD_AUTH"}'
 HEADER1='X-Amz-Target: AWSCognitoIdentityProviderService.InitiateAuth'
 HEADER2='Content-Type: application/x-amz-json-1.1'
-export STATUS_CODE=`curl -s  -o /dev/null -w "%{http_code}" -XPOST -H "$HEADER1" -H "$HEADER2" -d "$BODY" 'https://cognito-idp.ap-south-1.amazonaws.com/'`
-if [ $STATUS_CODE == 200 ]
+export IDTOKEN=curl -s -XPOST -H "$HEADER1" -H "$HEADER2" -d "$BODY" 'https://cognito-idp.ap-south-1.amazonaws.com/' | jq -r .AuthenticationResult.IdToken
+if [ $IDTOKEN == null ]
 then
+    echo "Not Authenticated"
+else
     echo "Authenticated user ..."
     echo "Started the downloading..."
-    export URL=`curl https://2jum04ptde.execute-api.ap-south-1.amazonaws.com/dev`
+    export URL=`curl -H "Authorization: $IDTOKEN" https://2jum04ptde.execute-api.ap-south-1.amazonaws.com/dev`
     curl -s --output myfile.sh $URL
-else
-    echo "Not Authenticated"
 fi
