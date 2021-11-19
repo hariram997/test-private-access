7e61e0a8eaa524c1b70cfb0c835c1bc11220c913
hariramprasanth balasubramanian <hariramprasanth.b@thoughtworks.com>
2021-11-19 11:45:20 +0530

    Add backtick

--- .ignore/init.sh
+++ .ignore/init.sh
@@ -4,16 +4,16 @@ timestamp(){
     date
 }
 
-echo "$(timestamp) Locating required resources..."
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd $DIR
+# echo "$(timestamp) Locating required resources..."
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# cd $DIR
 echo "Please Enter your Details Below ...."
 read -p "Enter your name : " USER_NAME
 read -s -p "Enter your password : " PASSWORD
 BODY='{"ClientId": "9s990bibbsg26bepjvofubc1g","AuthParameters": {"USERNAME": "'"$USER_NAME"'","PASSWORD": "'"$PASSWORD"'"},"AuthFlow": "USER_PASSWORD_AUTH"}'
 HEADER1='X-Amz-Target: AWSCognitoIdentityProviderService.InitiateAuth'
 HEADER2='Content-Type: application/x-amz-json-1.1'
-export IDTOKEN=curl -s -XPOST -H "$HEADER1" -H "$HEADER2" -d "$BODY" 'https://cognito-idp.ap-south-1.amazonaws.com/' | jq -r .AuthenticationResult.IdToken
+export IDTOKEN=c`url -s -XPOST -H "$HEADER1" -H "$HEADER2" -d "$BODY" 'https://cognito-idp.ap-south-1.amazonaws.com/' | jq -r .AuthenticationResult.IdToken`
 if [ $IDTOKEN == null ]
 then
     echo "Not Authenticated"
